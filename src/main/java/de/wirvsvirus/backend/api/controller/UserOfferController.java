9c84be465121ff4d37e43c7cda5ea601f19e63c6
slinde <sebastian.linde@adesso.de>
2020-03-21 14:09:56 +0100

    rm field-injection

--- src/main/java/de/wirvsvirus/backend/api/controller/UserOfferController.java
+++ src/main/java/de/wirvsvirus/backend/api/controller/UserOfferController.java
@@ -3,7 +3,7 @@ package de.wirvsvirus.backend.api.controller;
 import de.wirvsvirus.backend.api.exception.OfferNotFoundException;
 import de.wirvsvirus.backend.api.model.entity.UserOffer;
 import de.wirvsvirus.backend.api.repository.IUserOfferRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/offers")
+@RequiredArgsConstructor
 public class UserOfferController {
-    @Autowired
-    private IUserOfferRepository offerRepository;
+
+    private final IUserOfferRepository offerRepository;
 
     @GetMapping
     public Iterable findall() {
